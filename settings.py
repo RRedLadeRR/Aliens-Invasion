aaaac1e3ec6328578b750329785a9a37f9334ea0
RRedLadeRR <kuznetsovroman775@gmail.com>
2025-02-15 17:05:51 +0200

    Added settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Клас для зберігання всіх налаштуваннь гри"""
+
+    def __init__(self):
+        """Ініціалізує налаштування гри"""
+        # Параметри екрану
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (128, 0, 0)
