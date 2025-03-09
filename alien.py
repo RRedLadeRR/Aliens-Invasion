d2c41b49f11c52839b9bf390852bb9f15d28458a
RRedLadeRR <kuznetsovroman775@gmail.com>
2025-03-09 16:46:41 +0200

    Added alien size change

--- alien.py
+++ alien.py
@@ -1,6 +1,9 @@
 import pygame as pg
 from pygame.sprite import Sprite
 
+W = 64
+H = 64
+
 class Alien(Sprite):
     """Клас для прибульця"""
 
@@ -10,7 +13,7 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # Завантаження зображення прибульця та визначення rect
-        self.image = pg.image.load("images/tie-fighter.png")
+        self.image = pg.transform.scale(pg.image.load("images/tie-fighter.png"), (W, H))
         self.rect = self.image.get_rect()
 
         # Кожен новий прибулець з'являеться в лівому верхньому куті екрану
