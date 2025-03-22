8ea5b9b9da1f5b3f6debce49ad2218e92cac94e6
RRedLadeRR <kuznetsovroman775@gmail.com>
2025-03-22 17:02:27 +0200

    Added ship left on screen

--- ship.py
+++ ship.py
@@ -1,14 +1,16 @@
 import pygame as pg
 
+from pygame.sprite import Sprite
 
 W = 60
 H = 65.8
 
-class Ship:
+class Ship(Sprite):
     """Клас для управління кораблем"""
 
     def __init__(self, ai_game):
         """Ініціалізує корабель та встановлює цого початкову позицію"""
+        super().__init__()
         self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
